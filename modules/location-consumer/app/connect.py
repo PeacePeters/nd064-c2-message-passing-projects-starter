b7cd4711da245a50183e55f9ec549df96cfa0156
PeacePeters <peacepeters.pp@gmail.com>
2021-11-26 23:02:51 +0100

    Update changes

--- modules/location-consumer/app/connect.py
+++ modules/location-consumer/app/connect.py
@@ -1,58 +1,43 @@
+import psycopg2
+from kafka import KafkaConsumer
 import os
-from typing import List, Type
+import json
+
+kafka_topic = "locations"
+print('started listening ' + kafka_topic)
 
 DB_USERNAME = os.environ["DB_USERNAME"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
+KAFKA_URL = os.environ["KAFKA_URL"]
+
+kafka_consumer = KafkaConsumer(kafka_topic, bootstrap_servers=[KAFKA_URL])
 
 
-class BaseConfig:
-    CONFIG_NAME = "base"
-    USE_MOCK_EQUIVALENCY = False
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class DevelopmentConfig(BaseConfig):
-    CONFIG_NAME = "dev"
-    SECRET_KEY = os.getenv(
-        "DEV_SECRET_KEY", "You can't see California without Marlon Widgeto's eyes"
+def insert_location_to_db(location):
+    db_connection = psycopg2.connect(
+        dbname=DB_NAME,
+        port=DB_PORT,
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
     )
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    cursor = db_connection.cursor()
+    person_id = int(location["person_id"])
+    latitude, longitude = int(location["latitude"]), int(location["longitude"])
+    sql = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))".format(
+        person_id, latitude, longitude
     )
+    cursor.execute(sql)
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
 
-
-class TestingConfig(BaseConfig):
-    CONFIG_NAME = "test"
-    SECRET_KEY = os.getenv("TEST_SECRET_KEY", "Thanos did nothing wrong")
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
-
-
-class ProductionConfig(BaseConfig):
-    CONFIG_NAME = "prod"
-    SECRET_KEY = os.getenv("PROD_SECRET_KEY", "I'm Ron Burgundy?")
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
-
-
-EXPORT_CONFIGS: List[Type[BaseConfig]] = [
-    DevelopmentConfig,
-    TestingConfig,
-    ProductionConfig,
-]
-config_by_name = {cfg.CONFIG_NAME: cfg for cfg in EXPORT_CONFIGS}
+while True:
+    for location in kafka_consumer:
+        message = location.value.decode('utf-8')
+        print('{}'.format(message))
+        location_message = json.loads(message)
+        insert_location_to_db(location_message)    
